1130c88f70ce6c5c1352349437bc314684cb48f7
jhjeong <jhjeong@openit.co.kr>
2023-08-24 20:09:23 +0900

    initial

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './Main';
+
+
+export default function App() {
+  return (
+
+      <Main/>
+
+  );
+}
---
+++ screens/Home.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home2</Text>
+      <Text>Home</Text>
+      <Text>Home</Text>
+      <Text>Home</Text>
+      <Text>Home</Text>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
